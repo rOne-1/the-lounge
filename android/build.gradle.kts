2774f6036ee7a5fc9ebd3ade7452797ff7516da3
myhead2001 <myhead2001@gmail.com>
2026-08-10 01:42:58 +0530

    Configure Java 17 compatibility for Android builds
    Add automatic Kotlin plugin application for Android modules and configure Java 17 as the source and target compatibility level. Set JVM target to 17 for Kotlin compilation to ensure consistency across the build.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -16,21 +16,40 @@ subprojects {
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 subprojects {
+    plugins.withId("com.android.library") {
+        if (!plugins.hasPlugin("org.jetbrains.kotlin.android")) {
+            apply(plugin = "org.jetbrains.kotlin.android")
+        }
+    }
+    plugins.withId("com.android.application") {
+        if (!plugins.hasPlugin("org.jetbrains.kotlin.android")) {
+            apply(plugin = "org.jetbrains.kotlin.android")
+        }
+    }
     afterEvaluate {
         if (plugins.hasPlugin("com.android.library")) {
             extensions.configure<com.android.build.api.dsl.LibraryExtension> {
                 compileSdk = 36
+                compileOptions {
+                    sourceCompatibility = org.gradle.api.JavaVersion.VERSION_17
+                    targetCompatibility = org.gradle.api.JavaVersion.VERSION_17
+                }
             }
         }
         if (plugins.hasPlugin("com.android.application")) {
             extensions.configure<com.android.build.api.dsl.ApplicationExtension> {
                 compileSdk = 36
+                compileOptions {
+                    sourceCompatibility = org.gradle.api.JavaVersion.VERSION_17
+                    targetCompatibility = org.gradle.api.JavaVersion.VERSION_17
+                }
             }
         }
     }
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
         compilerOptions {
             languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
         }
     }
 }
@@ -38,6 +57,7 @@ subprojects {
 subprojects {
     project.evaluationDependsOn(":app")
 }
+
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
